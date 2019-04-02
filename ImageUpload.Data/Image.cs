4b242bee257627539525eb44390d84f367995022
Avrumi Friedman <info@lakewoodprogramming.com>
2019-04-01 21:50:56 -0400

    Initial commit

---
+++ ImageUpload.Data/Image.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUpload.Data
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public class ImageManager
+    {
+        private string _connectionString;
+
+        public ImageManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void SaveImage(Image image)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO Images (Description, FileName) " +
+                                  "VALUES (@desc, @fileName)";
+                cmd.Parameters.AddWithValue("@desc", image.Description);
+                cmd.Parameters.AddWithValue("@fileName", image.FileName);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<Image> Get()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var cmd = connection.CreateCommand())
+            {
+                var list = new List<Image>();
+                cmd.CommandText = "SELECT * FROM Images";
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(new Image
+                    {
+                        Id = (int)reader["Id"],
+                        Description = (string)reader["Description"],
+                        FileName = (string)reader["FileName"]
+                    });
+                }
+
+                return list;
+            }
+        }
+    }
+
+
+}
--- WebApplication14/Controllers/HomeController.cs
+++ WebApplication14/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using ImageUpload.Data;
 
 namespace WebApplication14.Controllers
 {
@@ -10,21 +12,30 @@ namespace WebApplication14.Controllers
     {
         public ActionResult Index()
         {
+            var mgr = new ImageManager(Properties.Settings.Default.ConStr);
+            var images = mgr.Get();
+            return View(images);
+        }
+
+        public ActionResult Upload()
+        {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase image, string description)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            string ext = Path.GetExtension(image.FileName);
+            string fileName = $"{Guid.NewGuid()}{ext}";
+            string fullPath = $"{Server.MapPath("/UploadedImages")}\\{fileName}";
+            image.SaveAs(fullPath);
+            var mgr = new ImageManager(Properties.Settings.Default.ConStr);
+            mgr.SaveImage(new Image
+            {
+                FileName = fileName,
+                Description = description
+            });
+            return Redirect("/");
         }
     }
 }
